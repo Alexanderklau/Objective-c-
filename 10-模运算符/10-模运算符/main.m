7ac8cd8b658a2d54d255f12e25a851c49501376c
Alex Lau <429095816@qq.com>
2016-07-08 20:32:31 +0800

    10

---
+++ 10-模运算符/10-模运算符/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  10-模运算符
+//
+//  Created by lwb on 16/7/8.
+//  Copyright © 2016年 lwb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int a= 25,b = 30,c = 40,d = 50;
+        NSLog(@"a %% b = %i",a % b);
+        NSLog(@"b %% c = %i",b % c);
+        NSLog(@"c %% d = %i",c % d);
+        
+    }
+    return 0;
+}
